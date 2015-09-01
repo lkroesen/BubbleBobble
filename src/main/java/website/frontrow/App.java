1efd0ae5bc82e6a299ea06c706d0f4e035545b5c
8uurg <arthur.guijt12@gmail.com>
2015-09-01 12:11:24 +0200

    Add default maven start template.

---
+++ src/main/java/website/frontrow/App.java
@@ -0,0 +1,13 @@
+package website.frontrow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
