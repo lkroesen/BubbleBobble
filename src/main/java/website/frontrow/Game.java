7a5cf0d0fb9b81023aaf50cafbea8e67b25468d9
lkroesen <l.kroesen@student.tudelft.nl>
2015-09-02 13:35:27 +0200

    Add Game Class

---
+++ src/main/java/website/frontrow/Game.java
@@ -0,0 +1,17 @@
+package website.frontrow;
+
+import website.frontrow.Level.Square;
+
+/**
+ * Created by lkroesen on 9/2/2015.
+ */
+public class Game
+{
+    int highscore;
+    Square[][] grid;
+
+    public Game()
+    {
+
+    }
+}
