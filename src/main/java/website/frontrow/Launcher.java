87dcfe6e2b809c9ec8663ea6a0e8abd98c02e2bc
8uurg <arthur.guijt12@gmail.com>
2015-09-02 11:24:00 +0200

    Add webhook for octopull.

---
+++ src/main/java/website/frontrow/Launcher.java
@@ -0,0 +1,12 @@
+package website.frontrow;
+
+/**
+ * Instantiates the game so it can be played.
+ */
+public class Launcher {
+
+    public static void main(String[] args)
+    {
+        // Main game entry point.
+    }
+}
