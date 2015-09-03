049300f22aaa5446efde5df4cbccfbf53a08a305
8uurg <arthur.guijt12@gmail.com>
2015-09-03 22:26:31 +0200

    Create level structure classes

---
+++ src/main/java/website/frontrow/level/Level.java
@@ -0,0 +1,22 @@
+package website.frontrow.level;
+
+import website.frontrow.board.Unit;
+import website.frontrow.util.Grid;
+
+import java.util.ArrayList;
+
+/**
+ * Class containing a level and positions of entities therein.
+ */
+public class Level {
+
+    private ArrayList<Unit> units;
+    private Grid<Cell> cells;
+
+    public Level(ArrayList<Unit> units, Grid<Cell> cells)
+    {
+        this.units = new ArrayList<Unit>(units);
+        this.cells = new Grid<Cell>(cells);
+    }
+
+}
---
+++ src/main/java/website/frontrow/util/Point.java
@@ -0,0 +1,16 @@
+package website.frontrow.util;
+
+/**
+ * A point or vector.
+ */
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+}
