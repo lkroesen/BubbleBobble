29346e67598d551452774e5bc3a1f06e43575d47
ThatFlint <remiflint@live.nl>
2015-09-08 08:58:14 +0200

    Added score to the Player class with getter and setter.

--- src/main/java/website/frontrow/board/Player.java
+++ src/main/java/website/frontrow/board/Player.java
@@ -9,11 +9,32 @@ public class Player extends Unit
 {
 
     /**
+     * The points accumulated by the player.
+     */
+    private int score;
+
+    /**
      * The constructor of the Player Unit.
      * Input a byte with the amount of lives the Player has.
      */
     public Player(Point position)
     {
         super(true, position, new Point(0, 0));
+    }
+
+    /**
+     * Returns the player's score
+     * @return score integer
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Adds p (points) to the score of the player, when it gets a pickup.
+     * @param p integer
+     */
+    public void addScore(int p) {
+        score += p;
     }
 }
