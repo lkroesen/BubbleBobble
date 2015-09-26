f06c7533664f2a6911cf6b6cb1bd230d7f3cef6e
lkroesen <L.Kroesen@student.tudelft.nl>
2015-09-26 19:25:35 +0200

    Remove abbreviations in MusicThread.

--- src/main/java/website/frontrow/util/MusicThread.java
+++ src/main/java/website/frontrow/util/MusicThread.java
@@ -10,27 +10,27 @@ import website.frontrow.logger.Logable;
  */
 public class MusicThread
         extends Thread
-            implements Logable
+        implements Logable
 {
     private Player music;
 
     /**
      * Start playing music.
-     * @param m
+     * @param musicPlayer
      * Input the music to be played.
      */
-    public void run(Player m)
+    public void run(Player musicPlayer)
     {
-        this.music = m;
+        this.music = musicPlayer;
         try
         {
             music.play();
         }
-        catch (Exception e)
+        catch (Exception exception)
         {
-            addToLog("[MT]\t[ERROR]\tMusic Thread encountered an exception: " + e.toString() + ".");
+            addToLog("[MT]\t[ERROR]\tMusic Thread encountered an exception: " + exception.toString() + ".");
             new DumpLog();
-            throw new RuntimeException("Error Occured in Thread " + e);
+            throw new RuntimeException("Error Occured in Thread " + exception);
         }
     }
 
