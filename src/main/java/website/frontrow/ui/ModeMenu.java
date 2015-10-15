bba8d2f81d5654b33a9063d2498926a81a142d44
8uurg <arthur.guijt12@gmail.com>
2015-10-15 22:49:28 +0200

    Fix Indentation
    < layout

--- src/main/java/website/frontrow/ui/ModeMenu.java
+++ src/main/java/website/frontrow/ui/ModeMenu.java
@@ -15,59 +15,59 @@ import website.frontrow.logger.Logable;
 import website.frontrow.util.FileNameCollector;
 
 public class ModeMenu
-			extends JFrame
-			implements Logable
+        extends JFrame
+        implements Logable
 {
-	
-	private final int width = 170;
-	private final int height = 150;
-	
-	public ModeMenu()
-	{
-		super("Bubble Bobble");
+
+    private final int width = 170;
+    private final int height = 150;
+
+    public ModeMenu()
+    {
+        super("Bubble Bobble");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         setFocusable(true);
-        
+
         Dimension size = new Dimension(width, height);
         this.setSize(size);
-        
+
         JLabel label1 = new JLabel("Choose A Mode:");
         add(label1, BorderLayout.PAGE_START);
-        
+
         Map<String, Action> buttonMappings = new HashMap<>();
         buttonMappings.put("Single Player Mode", () ->
         {
-            try 
+            try
             {
-				new Launcher().start(new FileNameCollector().obtain("level/"), 1);
-				setVisible(false);
-			} catch (Exception e) 
+                new Launcher().start(new FileNameCollector().obtain("level/"), 1);
+                setVisible(false);
+            } catch (Exception e)
             {
-				e.printStackTrace();
-			}
+                e.printStackTrace();
+            }
         });
-        
+
         buttonMappings.put("Multi Player Mode", () ->
         {
-            try 
+            try
             {
-				new Launcher().start(new FileNameCollector().obtain("level/"), 2);
-				setVisible(false);
-			} catch (Exception e) 
+                new Launcher().start(new FileNameCollector().obtain("level/"), 2);
+                setVisible(false);
+            } catch (Exception e)
             {
-				e.printStackTrace();
-			}
+                e.printStackTrace();
+            }
         });
-        
+
         ButtonPanel panel = new ButtonPanel(buttonMappings);
         add(panel, BorderLayout.CENTER);
-        
-		addToLog("[MODEMENU]\t created successfully.");
-		setVisible(true);
-	}
-	
-	/**
+
+        addToLog("[MODEMENU]\t created successfully.");
+        setVisible(true);
+    }
+
+    /**
      * Log actions taken by ModeMenu.
      * @param action Input a String that is the action performed.
      */
