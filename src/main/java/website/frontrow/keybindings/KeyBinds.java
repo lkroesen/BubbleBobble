71b2056fae4f6297ae0cdda748b130b1dd775ccc
Lars Stegman <L.S.Stegman@student.tudelft.nl>
2015-10-22 11:24:23 +0200

    Add accessors for key mappings.

--- src/main/java/website/frontrow/keybindings/KeyBinds.java
+++ src/main/java/website/frontrow/keybindings/KeyBinds.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 /**
  * Simple Class for keeping track of all keybindings.
+ *
+ * {@link java.awt.event.KeyEvent}
  */
 public abstract class KeyBinds
 {
@@ -29,7 +31,7 @@ public abstract class KeyBinds
      * The first key is the action type, the first integer is the player index,
      * the last is the key code.
      */
-    public static Map<ActionType, Map<Integer, Integer>> mapping;
+    private static Map<ActionType, Map<Integer, Integer>> mapping;
 
     // Static initializer, look it up.
     static
@@ -50,10 +52,69 @@ public abstract class KeyBinds
         shootMapping.put(0, KeyEvent.VK_ALT);
         shootMapping.put(1, KeyEvent.VK_SHIFT);
 
+        Map<Integer, Integer> printToggleMapping = new HashMap<>();
+        printToggleMapping.put(-1, KeyEvent.VK_F1);
+
+        Map<Integer, Integer> dumpLogMapping = new HashMap<>();
+        dumpLogMapping.put(-1, KeyEvent.VK_F2);
+
+        Map<Integer, Integer> volumeUpMapping = new HashMap<>();
+        volumeUpMapping.put(-1, KeyEvent.VK_EQUALS);
+
+        Map<Integer, Integer> volumeDownMapping = new HashMap<>();
+        volumeDownMapping.put(-1, KeyEvent.VK_MINUS);
+
         mapping = new HashMap<>();
         mapping.put(ActionType.JUMP, jumpMapping);
         mapping.put(ActionType.RIGHT, rightMapping);
         mapping.put(ActionType.LEFT, leftMapping);
         mapping.put(ActionType.SHOOT, shootMapping);
+        mapping.put(ActionType.TOGGLE_PRINT_LOG, printToggleMapping);
+        mapping.put(ActionType.STORE_LOG, dumpLogMapping);
+        mapping.put(ActionType.VOLUME_UP, volumeUpMapping);
+        mapping.put(ActionType.VOLUME_DOWN, volumeDownMapping);
+    }
+
+    /**
+     * Returns the key for the player for the specified action.
+     * @param action The action to perform.
+     * @param playerIndex The player to get the code for.
+     * @return The key code.
+     */
+    public static int getKeyCodeFor(ActionType action, int playerIndex)
+    {
+        return mapping.get(action).get(playerIndex);
+    }
+
+    /**
+     * Sets the key code for the specified action and player.
+     * @param action The action to perform.
+     * @param playerIndex The player to perform the action with.
+     * @param keyCode The key code.
+     */
+    public static void setKeyCodeFor(ActionType action, int playerIndex, int keyCode)
+    {
+        Map<Integer, Integer> actionMap = mapping.get(action);
+        actionMap.put(playerIndex, keyCode);
+    }
+
+    /**
+     * Returns the key code for a util action.
+     * @param action The action.
+     * @return The key code.
+     */
+    public static int getKeyCodeForUtil(ActionType action)
+    {
+        return getKeyCodeFor(action, -1);
+    }
+
+    /**
+     * Sets the key code for an util action.
+     * @param action The action.
+     * @param keyCode The new key code.
+     */
+    public static void setKeyCodeForUtil(ActionType action, int keyCode)
+    {
+        setKeyCodeFor(action, -1, keyCode);
     }
 }
