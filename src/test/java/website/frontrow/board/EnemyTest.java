6c579f5a08911a049084a73420d65710faf664cd
ThatFlint <remiflint@live.nl>
2015-10-23 13:19:02 +0200

    Added setter/getter tests for line coverage
    <testing

--- src/test/java/website/frontrow/board/EnemyTest.java
+++ src/test/java/website/frontrow/board/EnemyTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import website.frontrow.game.GameConstants;
 import website.frontrow.util.Point;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test the enemy class.
@@ -58,5 +61,139 @@ public class EnemyTest
         
         //An enemy might move
         assertEquals(0.25, u.getLocation().getX(), 2);
+    }
+    
+    /**
+     * Tests the setter for wallCollision.
+     */
+    @Test
+    public void testSetWallColision()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertFalse(enemy.getWallCollision());
+    	
+    	enemy.setWallCollision(true);
+    	assertTrue(enemy.getWallCollision());
+    }
+    
+    /**
+     * Tests the function onWallCollision.
+     */
+    @Test
+    public void testOnWallColision()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertFalse(enemy.getWallCollision());
+    	
+    	enemy.onWallCollision();
+    	assertTrue(enemy.getWallCollision());
+    }
+    
+    /**
+     * Tests the getter for tickCounter.
+     */
+    @Test
+    public void testGetTickCounter()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertEquals(enemy.getTickCounter(), 0);
+    }
+    
+    /**
+     * Tests the setter for tickCounter.
+     */
+    @Test
+    public void testSetTickCounter()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertEquals(enemy.getTickCounter(), 0);
+    	
+    	enemy.setTickCounter(10);
+    	assertEquals(enemy.getTickCounter(), 10);
+    }
+    
+    /**
+     * Tests the getter for random.
+     */
+	@Test
+    public void testGetRandom()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	// Not assertEquals, because that apparently doesn't 
+    	// accept two floats.
+    	assertTrue(enemy.getRandom() == 1.00f);
+    }
+	
+	/**
+     * Tests the setter for random.
+     */
+	@Test
+    public void testSetRandom()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	// Not assertEquals, because that apparently doesn't 
+    	// accept two floats.
+    	assertTrue(enemy.getRandom() == 1.00f);
+    	
+    	enemy.setRandom(2.00f);
+    	assertTrue(enemy.getRandom() == 2.00f);
+    }
+	
+	/**
+     * Tests the getter for lastWall.
+     */
+    @Test
+    public void testGetLastWall()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertFalse(enemy.getLastWall());
+    }
+	
+	/**
+     * Tests the setter for lastWall.
+     */
+    @Test
+    public void testSetLastWall()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertFalse(enemy.getLastWall());
+    	
+    	enemy.setLastWall(true);
+    	assertTrue(enemy.getLastWall());
+    }
+    
+    /**
+     * Tests the function getWorth().
+     */
+    @Test
+    public void testGetWorth()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	// DEFAULT_ENEMY_VALUE = 50.
+    	assertEquals(enemy.getWorth(), 50);
+    }
+    
+    /**
+     * Tests the getter for Mover player in enemy
+     */
+    @Test
+    public void testGetPlayer()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertNull(enemy.getPlayer());
+    }
+    
+    /**
+     * Tests the setter for Mover player in enemy
+     */
+    @Test
+    public void testSetPlayer()
+    {
+    	Enemy enemy = new Enemy(super.p, null);
+    	assertNull(enemy.getPlayer());
+    	
+    	Player player = new Player(new Point(0, 0), null);
+    	enemy.setPlayer((Mover) player);
+    	assertEquals(enemy.getPlayer(), (Mover) player);
     }
 }
