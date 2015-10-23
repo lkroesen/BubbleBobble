9c6477dba90705440bbff30d26355c4022a4176e
lkroesen <L.Kroesen@student.tudelft.nl>
2015-10-24 00:43:30 +0200

    Fix LauncherTest.
    <maybe,attempt2

--- src/test/java/website/frontrow/LauncherTest.java
+++ src/test/java/website/frontrow/LauncherTest.java
@@ -1,19 +1,37 @@
 package website.frontrow;
 
+import org.junit.Before;
 import org.junit.Test;
+import website.frontrow.keybindings.ActionType;
 import website.frontrow.keybindings.KeyBindFileHandler;
+import website.frontrow.keybindings.KeyBinds;
 import website.frontrow.util.FileNameCollector;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assume.assumeNotNull;
 
 /**
  * Test the launcher.
  */
 public class LauncherTest
 {
+    /**
+     * Run before LauncherTest to check for Nullpointers.
+     * @throws IOException Throws when triggered.
+     */
+    @Before
+    public void setup() throws IOException
+    {
+        KeyBindFileHandler.setFilename("src/test/resources/binds.txt");
+        KeyBindFileHandler.getInstance().loadBindings();
+
+        assumeNotNull("KeyBindings are null...",
+                KeyBinds.getKeyCodeFor(ActionType.JUMP, 0));
+    }
+
     /**
      * Test if the launcher is able to launch.
      * @throws IOException Throws when triggered.
@@ -23,8 +41,6 @@ public class LauncherTest
     public void testLaunch() throws IOException, URISyntaxException
     {
         Launcher launcher = new Launcher();
-        KeyBindFileHandler.setFilename("src/test/resources/binds.txt");
-        KeyBindFileHandler.getInstance().loadBindings();
         String[] files = FileNameCollector.getInstance().obtain("level/");
         launcher.start(files, 1);
         assertNotNull(launcher);
